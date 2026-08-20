45d54092c2d6eecda323ee4e089e5759d8c2ede6
Retno Anjarwati <anjarwati259@gmail.com>
2026-08-20 15:45:08 +0700

    test base

--- main.py
+++ main.py
@@ -24,8 +24,8 @@ parser.add_argument('--max_iter', type=int, default=5, help='Maximum iteration.'
 parser.add_argument('--ratio', type=str, default=30, help='Masking ratio.')
 parser.add_argument('--hid_dim', type=int, default=1024, help='Hidden dimension.')
 parser.add_argument('--mask', type=str, default='MCAR', help='Masking machenisms.')
-parser.add_argument('--num_trials', type=int, default=10, help='Number of sampling times.')
-parser.add_argument('--num_steps', type=int, default=50, help='Number of diffusion steps.')
+parser.add_argument('--num_trials', type=int, default=2, help='Number of sampling times.')
+parser.add_argument('--num_steps', type=int, default=20, help='Number of diffusion steps.')
 
 args = parser.parse_args()
 
