4ea47b13c8525427be61c177905bc182816117a9
Retno Anjarwati <anjarwati259@gmail.com>
2026-08-21 11:12:44 +0700

    revisi 2

--- main.py
+++ main.py
@@ -20,11 +20,11 @@ parser = argparse.ArgumentParser(description='Missing Value Imputation')
 parser.add_argument('--dataname', type=str, default='california', help='Name of dataset.')
 parser.add_argument('--gpu', type=int, default=0, help='GPU index.')
 parser.add_argument('--split_idx', type=int, default=0, help='Split idx.')
-parser.add_argument('--max_iter', type=int, default=6, help='Maximum iteration.')
+parser.add_argument('--max_iter', type=int, default=2, help='Maximum iteration.')
 parser.add_argument('--ratio', type=str, default=30, help='Masking ratio.')
 parser.add_argument('--hid_dim', type=int, default=1024, help='Hidden dimension.')
 parser.add_argument('--mask', type=str, default='MCAR', help='Masking machenisms.')
-parser.add_argument('--num_trials', type=int, default=10, help='Number of sampling times.')
+parser.add_argument('--num_trials', type=int, default=2, help='Number of sampling times.')
 parser.add_argument('--num_steps', type=int, default=50, help='Number of diffusion steps.')
 parser.add_argument('--reset', action='store_true', help='Hapus checkpoint lama sebelum training.')
 parser.add_argument('--save_csv', type=str, default='True', choices=['True', 'False'],
@@ -137,7 +137,7 @@ if __name__ == '__main__':
             generator=generator  # Gunakan GPU generator
         )
 
-        num_epochs = 10000 + 1
+        num_epochs = 100 + 1
 
         denoise_fn = MLPDiffusion(in_dim, hid_dim).to(device)
 
